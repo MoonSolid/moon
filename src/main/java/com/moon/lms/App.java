ad9cb4a36b2111c5a37381eb02d4d27bc27abf15
MoonSolid <rnreo20@gmail.com>
2020-01-15 01:51:41 +0900

    v005

--- src/main/java/com/moon/lms/App.java
+++ src/main/java/com/moon/lms/App.java
@@ -1,13 +1,50 @@
 package com.moon.lms;
 
+import java.sql.Date;
+import java.util.Scanner;
 public class App {
    public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("수업명:자바 프로젝트 실습");
-    System.out.println("설명: 자바 프로젝트를 통한 자바 언어 활용법 익히기");
-    System.out.println("시작일: 2019-01-02");
-    System.out.println("종료일: 2019-05-28");
-    System.out.println("총수업시간: 1000시간");
-    System.out.println("일수업시간: 8 시간");
+     Scanner keyboard = new Scanner(System.in);
+     
+     
+    System.out.print("번호?");
+    int no = keyboard.nextInt();
+    keyboard.nextLine();
+    
+    System.out.print("수업명?");
+    String title = keyboard.nextLine();
+    
+    System.out.print("설명?");
+    String description = keyboard.nextLine();
+    
+    System.out.print("시작일?");
+    Date startDate = Date.valueOf(keyboard.next());
+    
+    System.out.print("종료일?");
+    Date endDate = Date.valueOf(keyboard.next());
+    
+    
+    System.out.print("총수업시간?");
+    int totalHours = keyboard.nextInt();
+    
+    
+    System.out.print("일수업시간?");
+    int dayHours = keyboard.nextInt();
+    
+    System.out.println();
+    
+    System.out.printf("번호: %d\n", no);
+    System.out.printf("수업명: %s\n", title);
+    System.out.printf("설명: %s\n", description);
+    System.out.printf("기간: %s ~ %s\n", startDate, endDate);
+    System.out.printf("총수업시간: %d\n", totalHours);
+    System.out.printf("일수업시간: %d\n", dayHours);
+    keyboard.close();
   }
 }
+
+
+
+
+
+
--- src/main/java/com/moon/lms/App3.java
+++ src/main/java/com/moon/lms/App3.java
@@ -1,10 +1,29 @@
 package com.moon.lms;
 
+import java.sql.Date;
+import java.util.Scanner;
+
 public class App3 {
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("내용: 게시글입니다.");
-    System.out.println("작성일: 2019-01-01");
-    System.out.println("조회수: 0");
+   Scanner keyboard = new Scanner(System.in);
+   
+   System.out.print("번호?");
+   int no = keyboard.nextInt();
+   keyboard.nextLine();
+   
+   System.out.print("내용?");
+   String title = keyboard.nextLine();
+   
+   Date today = new Date(System.currentTimeMillis());
+   int count = 0;
+   
+   System.out.println();
+   
+   System.out.printf("번호: %d\n", no);
+   System.out.printf("내용: %s\n", title);
+   System.out.printf("작성일: %s\n", today);
+   System.out.printf("조회수: %d\n", count);
+   
+   keyboard.close();
   }
 }
